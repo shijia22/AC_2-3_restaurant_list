b9a777f9d1fe13e3c33b3cd5f6d527c16213e34f
shichia22 <shichia0207@gmail.com>
2021-06-27 20:13:44 +0800

    載入 bootstrap & index.css

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const exphbs = require('express-handlebars')
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+// setting static files
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.render('index')
 })
