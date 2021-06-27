5c739ffe1f614c2188bddfea9b5efbe97d68e7d6
shichia22 <shichia0207@gmail.com>
2021-06-27 19:51:52 +0800

    修正資料夾多一個空白產生的錯誤、載入樣板引擎

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const exphbs = require('express-handlebars')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
 app.get('/', (req, res) => {
-  res.send('test 123')
+  res.render('index')
 })
 
 app.listen(port, () => {
