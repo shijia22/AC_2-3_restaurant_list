9e7ef2c89aff120c23ed1dcdbf96193f25a935a4
shichia22 <shichia0207@gmail.com>
2021-06-28 18:30:45 +0800

    新增 搜尋功能

--- app.js
+++ app.js
@@ -4,13 +4,11 @@ const port = 3000
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
 
-
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 // setting static files
 app.use(express.static('public'))
-
 
 app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantList.results })
@@ -19,8 +17,16 @@ app.get('/', (req, res) => {
 app.get('/restaurants/:restaurant_id', (req, res) => {
   const restaurant = restaurantList.results.find(
     (restaurant) => restaurant.id.toString() === req.params.restaurant_id
-    )
+  )
   res.render('show', { restaurant: restaurant })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter((restaurant) => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', { restaurants: restaurants, keyword: keyword })
 })
 
 app.listen(port, () => {
