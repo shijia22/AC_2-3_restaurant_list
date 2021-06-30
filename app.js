4680195fbb2a518b32c79e2a7ffea9145044af8e
shichia22 <shichia0207@gmail.com>
2021-06-30 12:23:18 +0800

    將 餐廳分類篩選 加入

--- app.js
+++ app.js
@@ -24,7 +24,12 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
   const restaurants = restaurantList.results.filter((restaurant) => {
-    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+    return (
+      restaurant.name.toLowerCase().includes(keyword.toLowerCase()) ||
+      restaurant.category
+        .toLocaleLowerCase()
+        .includes(keyword.toLocaleLowerCase())
+    )
   })
   res.render('index', { restaurants: restaurants, keyword: keyword })
 })
