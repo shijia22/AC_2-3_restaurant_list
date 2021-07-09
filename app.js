54604a2c4728be3ff802e11851ea7d161f207f34
shichia22 <shichia0207@gmail.com>
2021-07-09 22:05:48 +0800

    feat: add delete function

--- app.js
+++ app.js
@@ -125,6 +125,15 @@ app.post('/restaurants/:id', (req, res) => {
     .catch((error) => console.error(error))
 })
 
+// delete
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then((restaurant) => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch((error) => console.log(error))
+})
+
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
 })
