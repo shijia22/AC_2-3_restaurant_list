461d48d1d1eb370394fd34a434ec8d833f5d1904
shichia22 <shichia0207@gmail.com>
2021-07-09 14:33:31 +0800

    updata seeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
+
 const Restaurant = require('../restaurant')
-const restaurantList = require('./restaurant.json')
+const data = require('./restaurant.json') // 命名變數，便於維護
+const restaurantList = data.results //  載入 JSON
 
 mongoose.connect('mongodb://localhost/menu', {
   useNewUrlParser: true,
@@ -15,6 +17,20 @@ db.on('error', () => {
 })
 db.once('open', () => {
   console.log('mongodb connected!')
-  Restaurant.create(restaurantList.results)
+  // 建立資料，定義內容
+  restaurantList.forEach((restaurant) =>
+    Restaurant.create({
+      id: restaurant.id,
+      name: restaurant.name,
+      name_en: restaurant.name_en,
+      category: restaurant.category,
+      image: restaurant.image,
+      location: restaurant.location,
+      phone: restaurant.phone,
+      google_map: restaurant.google_map,
+      rating: restaurant.rating,
+      description: restaurant.description,
+    })
+  )
   console.log('done.')
 })
